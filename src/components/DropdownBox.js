c105ea51c1c53f21643d40983f970d44966e6442
dongb909 <dongb909@gmail.com>
2021-04-27 13:42:34 -0700

    Create Routes dropdown feature

--- src/components/DropdownBox.js
+++ src/components/DropdownBox.js
@@ -1,17 +1,47 @@
-import React from 'react';
+import React from "react";
 
-const DropdownBox = ({dataList, getData, dataType, title, selectedItem, setSelected}) =>{
-  const options = dataList.map(data => <option value={data[0]} key={data[1]}>{data[0]}</option>);
-  
- return(
-   <div className="">
-       <select name={dataType} value={selectedItem} onChange={(e) =>{getData(e.target.key)
-        setSelected(e.target.value)} }>
-         <option value={`Select a ${title}`}></option>
-         {options}
-       </select>
-   </div>
- )
-}
+const DropdownBox = ({
+	dataList,
+	getData,
+	dataType,
+	title,
+	selectedRoute,
+	setSelectedRoute,
+}) => {
+	const options = dataList.map((data) => (
+		<option value={data[0]} key={data[1]}>
+			{data[0]}
+		</option>
+	));
+
+	const [isDisplay, toggleDisplay] = useState(false);
+
+	const toggleHandler = () => toggleDisplay(!isDisplay);
+
+	return (
+		<div>
+			<Button onClick={toggleHandler}>{selectedRoute[0] || ""}</Button>
+			<ul
+				className={classNames({
+					"display-none": !isDisplay,
+				})}
+			>
+				{dataList.map((data) => (
+					<li key={data[0] + "-" + data[1]}>
+						<button
+							onClick={(e) => {
+								e && e.preventDefault();
+								toggleHandler();
+								setSelectedRoute(data);
+							}}
+						>
+							{data[0]} - {data[1]}
+						</button>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
+};
 
 export default DropdownBox;
--- src/components/RouteOptions.js
+++ src/components/RouteOptions.js
@@ -1,16 +1,27 @@
-import React from 'react';
-import DropdownBox from './DropdownBox';
+import React from "react";
+import DropdownBox from "./DropdownBox";
 
-const RouteOptions = ({metroRoutes, getDirections, setSelectedRoute, selectedRoute}) =>{
-  const routeNames = metroRoutes.map(({route_label, route_id})=> [route_label, route_id])
-  console.log(metroRoutes)
- //  const getRouteId =(e) => {
- //   const selectedId = e.target.val
- //   getDirections(selectedId)
- // }
- return(
-   <DropdownBox dataList={routeNames} dataType="Routes" title="route" getData={getDirections} setSelected={setSelectedRoute} selectedItem={selectedRoute}/>
- )
-}
+const RouteOptions = ({
+	metroRoutes,
+	getDirections,
+	setSelectedRoute,
+	selectedRoute,
+}) => {
+	const routeNames = metroRoutes.map(({ route_label, route_id }) => [
+		route_label,
+		route_id,
+	]);
+
+	return (
+		<DropdownBox
+			dataList={routeNames}
+			dataType="Routes"
+			title="route"
+			getData={getDirections}
+			setSelectedRoute={setSelectedRoute}
+			selectedRoute={selectedRoute}
+		/>
+	);
+};
 
 export default RouteOptions;
