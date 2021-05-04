0c04f174464853c82700745ee6f8d99eef5bb883
dongb909 <dongb909@gmail.com>
2021-05-04 09:29:05 -0700

    Cleanup

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
 const Dropdown = ({
 	data,
@@ -8,7 +9,7 @@ const Dropdown = ({
 }) => {
 	return (
 		<div className="mt-10 mb-10 ">
-			<select
+			<select 
 				data-testid={dataTestId}
 				className="border border-gray-300 rounded-lg text-gray-600 h-10 w-80 pl-5 pr-10 bg-white hover:border-gray-400 focus:outline-none appearance-none overflow-auto "
 				onChange={(e) => {
@@ -31,4 +32,18 @@ const Dropdown = ({
 	);
 };
 
+Dropdown.propTypes={
+	data: PropTypes.arrayOf(PropTypes.shape({
+			elementName: PropTypes.string,
+			elementID: PropTypes.string,
+			elementKey: PropTypes.string
+	})),
+	onChangeCallback: PropTypes.func,
+	"data-testid": PropTypes.string,
+	dropType: PropTypes.string
+}
+
+
 export default Dropdown;
+
+
